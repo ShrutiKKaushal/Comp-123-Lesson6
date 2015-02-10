bc857905969946f1fe3515909537f5a5794afece
akashruti <shruti_01@live.com>
2015-02-10 13:20:23 -0500

    Added Instance variables , Properties and accessors

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -10,7 +10,17 @@ namespace Lesson6
     {
         static void Main(string[] args)
         {
+            Class1 myClass = new Class1("Hello");
 
+            myClass.IntegerNumber = 10;
+            myClass.DoubleNumber = 50.0;
+
+            myClass.show();
+
+            Console.Write("\n");
+            Console.WriteLine("+++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
